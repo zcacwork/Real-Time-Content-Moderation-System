d2c4e0995feab38189107b942560aa713844b90d
Priyanshu Kumar Jangir <zcacwork@gmail.com>
2026-04-22 22:33:25 +0530

    Load data and split into train/validation sets
    Load training data and split into training and validation sets.

--- src/train.py
+++ src/train.py
@@ -4,3 +4,9 @@ from torch.utils.data import Dataset, DataLoader
 from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
 from sklearn.model_selection import train_test_split
 from preprocess import load_data
+
+df = load_data("../data/train.csv")
+
+train_texts, val_texts, train_labels, val_labels = train_test_split(
+    df['comment_text'], df['toxic_label'], test_size=0.1
+)
