40437784a626f473f1958c81b04470b50655a3af
Priyanshu Kumar Jangir <zcacwork@gmail.com>
2026-04-23 22:20:12 +0530

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+def load_data(path):
+    df = pd.read_csv(path)
+
+    # Binary classification (toxic vs non-toxic)
+    df['toxic_label'] = df[['toxic','severe_toxic','obscene','threat','insult','identity_hate']].max(axis=1)
+
+    df = df[['comment_text', 'toxic_label']]
+    df = df.dropna()
+
+    return df
