8e3a690717831323daf8aa7b3418adafab805c0a
Priyanshu Kumar Jangir <zcacwork@gmail.com>
2026-04-23 22:18:20 +0530

    Load dataset and initialize tokenizer in train.py

--- src/train.py
+++ src/train.py
@@ -5,8 +5,16 @@ from transformers import DistilBertTokenizer, DistilBertForSequenceClassificatio
 from sklearn.model_selection import train_test_split
 from preprocess import load_data
 
+# Load dataset
 df = load_data("../data/train.csv")
 
 train_texts, val_texts, train_labels, val_labels = train_test_split(
     df['comment_text'], df['toxic_label'], test_size=0.1
 )
+
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+
+class ToxicDataset(Dataset):
+    def __init__(self, texts, labels):
+        self.encodings = tokenizer(list(texts), truncation=True, padding=True)
+        self.labels = list(labels)
