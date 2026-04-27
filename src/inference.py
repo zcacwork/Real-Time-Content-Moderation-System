5c05be5788da7683ca0527f077d35175d27bc071
Priyanshu Kumar Jangir <zcacwork@gmail.com>
2026-04-27 23:59:13 +0530

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -3,3 +3,12 @@ import onnxruntime as ort
 import numpy as np
 from transformers import DistilBertTokenizer
 
+import onnxruntime as ort
+import numpy as np
+from transformers import DistilBertTokenizer
+
+tokenizer = DistilBertTokenizer.from_pretrained("../models/")
+session = ort.InferenceSession("../models/model.onnx")
+
+def predict(text):
+    inputs = tokenizer(text, return_tensors="np", truncation=True, padding=True, max_length=128)
